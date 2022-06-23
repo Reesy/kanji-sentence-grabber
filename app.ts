e747e70cc2cdba069e0347b2d1dc176bf2588f65
James Rees <James.Rees@outlook.com>
2022-06-23 15:52:00 +0100

    Still experimenting with the correct approach, I'm leaning towards the current way

--- app.ts
+++ app.ts
@@ -8,17 +8,48 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/api/japanese-english', async (req: express.Request, res: express.Response) =>
-{ 
+{
 
   await grabber("test");
   res.json({ message: 'Hello World!' });
 });
 
+
+
+interface sentenceBody
+{
+  jp: string;
+  en: string;
+  romaji: string;
+};
+
+
+let englishSentences: string[] = [];
+let japaneseSentences: string[] = [""];
+let romajiSentences : string[] = [];
+
 app.get('/api/english-japanese', async (req: express.Request, res: express.Response) =>
-{ 
+{
 
   let response = await axios.get("https://tangorin.com/sentences?search=%E7%B5%B1%E8%A8%88");
   let $ = cheerio.load(response.data);
+
+  $("#App > main > div.ResultsWrapper > div.results-main-container > section > div > dl").children()
+    .each(
+      (i, sentenceCollection) =>
+      {
+        $(sentenceCollection).children().each((y, sentenceContainer: any) =>
+        {
+          if (sentenceContainer.attribs.class === "entry-menu-wrap")
+          {
+            return;
+          }
+          sentenceExtractor(sentenceContainer);
+        });
+        // console.log(i);
+        // console.log('elem', $(elem).text());
+
+      });
 
   let sentences = $("#App > main > div.ResultsWrapper > div.results-main-container > section > div > dl > div:nth-child(1) > dt")
   let translations = $("#App > main > div.ResultsWrapper > div.results-main-container > section > div > dl > div:nth-child(1) > dt > a:nth-child(1) > ruby")
@@ -26,6 +57,7 @@ app.get('/api/english-japanese', async (req: express.Request, res: express.Respo
   let sentence = sentences.text();
   let translation = translations.text();
 
+  console.log(japaneseSentences);
   res.json({ sentence: sentence, translation: translation });
 
 
@@ -36,6 +68,23 @@ let grabber = async (word: string) =>
   let $ = cheerio.load(response.data);
   return response;
 }
+
+// A function named sentenceExtractor that returns a sentenceBody object.
+let sentenceExtractor = (html: string) =>
+{
+
+  
+
+  let transientSentenceBody: sentenceBody = 
+  { 
+    jp: "shouldBeJP", 
+    en: "shouldBeEn", 
+    romaji: "shouldBeRomaji"
+  };
+
+    return transientSentenceBody;
+}
+
 
 
 export { app };
